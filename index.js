43b0c86f5c0418112ef236f135c58c15933eacd1
JAFFAR NGAIRA <garsolinajaffar@gmail.com>
2023-03-31 17:28:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun (hobby="roller-skate"){
+    return `This Saturday, I want to ${hobby}!`
+
+}
+function mondayWork(office="go to the office"){
+    return  `This Monday, I will ${office}.`
+
+}
+function wrapAdjective(adj="*"){
+    const func=function(adjective="special"){
+        return `You are ${adj}${adjective}${adj}!` 
+    }
+    return func
+}
+
+
+
+
+
+
+
+
+
